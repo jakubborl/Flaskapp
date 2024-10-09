6631f611439f5a41866ad6f98d3afacdc8ad35da
jakubborl <jakub.borl@icloud.com>
2024-10-09 19:48:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,7 @@
-from website import create_app
+from website import create_app, db
 from flask_migrate import Migrate
 from website.views import views as views_blueprint
-from website import db
-from website import db
-from website import create_app, db 
-
-
 
 app = create_app()
 
 migrate = Migrate(app, db)
-
-if __name__ == "__main__":
-    app.run(debug=True, port=8080)
